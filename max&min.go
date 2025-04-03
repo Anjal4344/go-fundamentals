0174b7782c3337ef1300df77afeff509e8a9fba2
Anjal4344 <anjalkarad4344@gmail.com>
2025-04-03 17:05:35 +0530

    the max and min values in an array (OG)

--- max&min.go
+++ max&min.go
@@ -3,9 +3,6 @@ package main
 import "fmt"
 
 func maxmin(arr []int) (int, int) {
-	if len(arr) == 0 {
-		panic("ARRAY CAN'T BE EMPTY")
-	}
 	min0, max0 := arr[0], arr[0]
 	for _, num := range arr {
 		if num < min0 {
@@ -17,15 +14,18 @@ func maxmin(arr []int) (int, int) {
 	}
 	return min0, max0
 }
+
 func main() {
-	var ARRAY []int
-	fmt.Print("Enter number of elements of array =")
-	_, err := fmt.Scan(&ARRAY)
-	if err != nil {
-		fmt.Println("Error reading arrray =", ARRAY)
-		return
+	a := make([]int, 5)
+	fmt.Print("Enter 5 numbers of elements of array: ")
+	for i := 0; i < 5; i++ {
+		_, err := fmt.Scan(&a[i])
+		if err != nil {
+			fmt.Println("type integer", err)
+			return
+		}
 	}
-	min1, max1 := maxmin(ARRAY)
-	fmt.Printf("max = %.2d\n", max1)
-	fmt.Printf("min = %.2d\n", min1)
+	min1, max1 := maxmin(a)
+	fmt.Printf("max = %d\n", max1)
+	fmt.Printf("min = %d\n", min1)
 }
